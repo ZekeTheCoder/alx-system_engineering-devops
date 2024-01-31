f45100b147f2038d58c0ea6b38d476d1f959dd2e
Thabang Maphangisane <thabangh647@gmail.com>
2024-01-31 23:58:07 +0200

    1. Repetition Token #0

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# This is a Ruby script that accepts one argument and pass it to a regular expression that matches the following cases:
+# hbn
+# hbtn
+# hbttn
+# hbtttn
+# hbttttn
+# hbtttttn
+# hbttttttn
+
+puts ARGV[0].scan(/hbt*n/).join
