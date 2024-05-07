2e37cfbc992eaf4b32ba455b7ec7b3bec26a5507
Thabang Maphangisane <thabangh647@gmail.com>
2024-05-07 21:42:30 +0200

    0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    This function returns the number of subscribers for a given subreddit.
+    """
+    URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'User-Agent': 'ZekeTheCoder/1.0'}
+    response = requests.get(URL, headers=header, allow_redirects=False)
+
+    if response.status_code == 200:
+        return (response.json()['data']['subscribers'])
+    else:
+        return (0)
