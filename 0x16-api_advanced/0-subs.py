c786c67b814631b9ff5b091b55167336f5594755
Thabang Maphangisane <thabangh647@gmail.com>
2024-05-07 22:52:17 +0200

    0. How many subscribers? - try catch

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,6 +15,15 @@ def number_of_subscribers(subreddit):
 
     URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     header = {'User-Agent': 'ZekeTheCoder'}
-    response = requests.get(URL, headers=header, allow_redirects=False).json()
-    number_of_subscribers = response.get("data", {}).get("subscribers", 0)
-    return (number_of_subscribers)
+
+    try:
+        response = requests.get(URL, headers=header, allow_redirects=False)
+        response.raise_for_status()
+        number_of_subscribers = response.json().get("data", {}).get("subscribers", 0)
+        return (number_of_subscribers)
+    
+    except requests.exceptions.RequestException:
+        return 0
+
+    except (KeyError, ValueError):
+        return 0
