03523260712d98a2fd9d43e8b999d958cfb4ca09
Thabang Maphangisane <thabangh647@gmail.com>
2024-05-08 19:44:56 +0200

    fix: 0. Strace is your friend - puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
  # Using strace, find out and fix Apache 500 error using Puppet.
 
 exec {'fix_typo':
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path => ['/bin','/usr/bin']
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/bin','/usr/bin']
 }
